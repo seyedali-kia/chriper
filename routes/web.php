e25e4ed9718b9e3b2560abdf95f488e925981d11
Seyed Ali Kia <seyedalikia8383@gmail.com>
2025-11-20 16:56:37 +0330

    adding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,43 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\ChirpController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function(){
+
+    Route::post('/chirps',[ChirpController::class, 'store']);
+    Route::get('chirps/{chirp}/edit',[ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}',[ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}',[ChirpController::class, 'destroy']);
+
 });
+
+
+
+
+// REGISTER ROUTES
+Route::view('/register','auth.register')
+    ->Middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class);
+
+// LOGOUT
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
+
+// LOGIN
+Route::view('/ogin', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('login',Login::class)
+    ->middleware('guest');
